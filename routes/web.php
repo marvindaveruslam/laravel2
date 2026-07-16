086e89c5b6951f332702f8f953040dfae9ca8523
Adi <adiwiranata2009@gmail.com>
2026-07-16 11:44:31 +0800

    Menambahkan dashboard sama test

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
+
+    Route::get('/test', function () {
+        return view('test');
+    })->name('test');
