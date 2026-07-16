9a144b67599a6d8720b0779de8edc2bf416e2bfd
Adi <adiwiranata2009@gmail.com>
2026-07-16 11:54:33 +0800

    menghapus dashboard dan test

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::get('/test', function () {
-        return view('test');
-    })->name('test');
